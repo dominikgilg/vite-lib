24d864bf7eeaf8b23ba6abf33fd70d0d1bd74f71
Dominik Gilg <dominikgilg@Dominiks-MacBook-Pro.local>
2022-12-14 13:52:06 +0100

    create lib

---
+++ src/components/Test.client.jsx
@@ -0,0 +1,5 @@
+function Test({ children }) {
+  return <div className="test-dom">{children}</div>;
+}
+
+export default Test;
---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.js"),
+      name: "ViteLib",
+      fileName: "vite-lib",
+    },
+    external: ["react", "react-dom", "@shopify/hydrogen"],
+    output: {
+      globals: {
+        react: "React",
+        "react-dom": "ReactDOM",
+      },
+    },
+  },
+});
