c74cba3626543c562ead60a85b36b94c3c26bf41
Dominik Gilg <dominikgilg@Dominiks-MacBook-Pro.local>
2022-12-14 17:55:49 +0100

    updated lib

--- src/components/Test.client.jsx
+++
@@ -1,5 +0,0 @@
-function Test({ children }) {
-  return <div className="test-dom">{children}</div>;
-}
-
-export default Test;
--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,29 @@
 import react from "@vitejs/plugin-react";
 import path from "node:path";
 import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
   build: {
     lib: {
-      entry: path.resolve(__dirname, "src/index.js"),
-      name: "@nosto/nosto-react",
+      entry: path.resolve(__dirname, "src/lib/index.ts"),
+      name: "MyLib",
       formats: ["es", "umd"],
-      fileName: (format) => `index.${format}.js`,
+      fileName: (format) => `my-lib.${format}.js`,
     },
     rollupOptions: {
-      external: ["react", "react-dom", "snakize"],
+      external: ["react", "react-dom", "styled-components"],
       output: {
         globals: {
           react: "React",
           "react-dom": "ReactDOM",
+          "styled-components": "styled",
         },
       },
     },
