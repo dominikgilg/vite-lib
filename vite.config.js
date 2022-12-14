e8cb2174018aebf107f0e130f64fcaed47d11785
Dominik Gilg <dominikgilg@Dominiks-MacBook-Pro.local>
2022-12-14 15:52:23 +0100

    .

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
-import path from "path";
-
-console.log(__filename, __dirname, import.meta.url);
 
 export default defineConfig({
   plugins: [react()],
